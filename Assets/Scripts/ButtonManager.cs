dbd9b62dc23af1d09a10f98891e065108c7394a5
Doğa Melis ERKE <dogaerke2001@gmail.com>
2024-11-02 21:04:00 +0300

    Created projectr and add some UI elements

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ButtonManager : MonoBehaviour
+{
+    private int _number1 = 0;
+    private int _number2 = 0;
+    private int _number3 = 0;
+    private Button _button;
+
+    private void WriteNumbers()
+    {
+        
+        
+        
+    }
+
+    private void SaveNumber()
+    {
+        
+        
+    }
+}
